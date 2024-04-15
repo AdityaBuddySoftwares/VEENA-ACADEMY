8229cb18d8828a786b0a119da5300a5885d55a98
AdityaBuddy <adityabuddy.softwares@gmail.com>
2024-04-15 14:58:03 +0530

    initial commit

---
+++ src/HomePagecomponents/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div>
+
+
+   
+ 
+        <header className="alm">
+        <nav className="gx lx ua yz zf aqu dde" aria-label="Global"><a href="#" className="fr aqm">
+            <span className="t">Your Company</span>
+            <img className="og tm" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&amp;shade=600" alt=""/>
+            </a>
+        <div className="lx cuz">
+            <button type="button" className="ft ly yz ze adu aqq axs">
+                <span className="t">Open main menu</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" className="oc se">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+                </svg>
+                </button>
+        </div>
+        <div className="md cuv czq"><a href="#" className="avz awf awo axu">Product</a><a href="#" className="avz awf awo axu">Features</a><a href="#" className="avz awf awo axu">Marketplace</a><a href="#" className="avz awf awo axu">Company</a><a href="#" className="avz awf awo axu">Log in <span aria-hidden="true">→</span></a>
+        </div></nav>
+        <div className="position: fixed; top: 1px; left: 1px; width: 1px; height: 0px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; border-width: 0px; display: none;">
+            </div>
+        </header>
+        </div>
+        </>
+  )
+}
+
+export default Header
